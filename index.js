4c8c5f8dd871fea60acf6d5b945346e6f0057b92
John Khachian <jaksawesome10@gmail.com>
2023-02-10 15:38:14 -0500

    initial code

---
+++ index.js
@@ -0,0 +1,18 @@
+// A express server, which will handle api requests coming in and respond back with a json object, it will use body parser as well as cors
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
