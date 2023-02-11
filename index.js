811676baac1b3c611d135a6abca07a744f1ce338
John Khachian <jaksawesome10@gmail.com>
2023-02-10 19:00:39 -0500

    openai api setup

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 // A express server, which will handle api requests coming in and respond back with a json object, it will use body parser as well as cors
 
+const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = OpenAI;
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 3001;
+require('dotenv').config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        maxTokens: 7,
+        temperature: 0
+    });
+
+    console.log(response);
     res.json({
         message: 'Hello World'
     });
